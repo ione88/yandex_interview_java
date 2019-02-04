d18a67d63ca6b2f063afe687d75aa7feb5c70414
Ivan <Alekseev_ivan@mail.ru>
2019-02-04 12:17:41 +0300

    Update TaskCSolution.java

--- TaskCSolution.java
+++ TaskCSolution.java
@@ -1,11 +1,9 @@
 /*
 Oracle Java 8
 1 секунда	20Mb
-
 Дан упорядоченный по неубыванию массив целых 32-разрядных чисел. Требуется удалить из него все повторения. 
 Желательно получить решение, которое не считывает входной файл целиком в память, т.е., использует лишь константный объем памяти в процессе работы.
 Формат ввода Первая строка входного файла содержит единственное число n, n ≤ 1000000.
-
 На следующих n строк расположены числа — элементы массива, по одному на строку. Числа отсортированы по неубыванию.
 Формат вывода Выходной файл должен содержать следующие в порядке возрастания уникальные элементы входного массива.
 Пример 1 
@@ -28,39 +26,29 @@ Oracle Java 8
 
 import java.io.*;
 
-public class WooHoo {
+public class TaskCSolution {
+    private static final String FILE_INPUT = "input.txt";
+    private static final String FILE_OUTPUT = "output.txt";
     public static void main(String[] args) throws Exception {
-        FileInputStream r  = new FileInputStream("input.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_INPUT));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(FILE_OUTPUT));
 
-        int n = Integer.valueOf(readLine(r));
+        int n = Integer.valueOf(bufferedReader.readLine());
         if (n < 1) return;
-        int m = Integer.valueOf(readLine(r));
-        System.out.println(m);
+        int m = Integer.valueOf(bufferedReader.readLine());
+        bufferedWriter.write(String.valueOf(m));
+        bufferedWriter.newLine();
         int l = m;
-
         for (int i = 1; i < n; ++i) {
-            m = Integer.valueOf(readLine(r));
+            m = Integer.valueOf(bufferedReader.readLine());
             if (m != l) {
-                System.out.println(m);
+                bufferedWriter.write(String.valueOf(m));
+                bufferedWriter.newLine();
                 l = m;
             }
+            if (i%33334==33333)  Runtime.getRuntime().gc();
+
         }
-    }
-
-    public static String readLine(InputStream inputStream) throws IOException {
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        int r;
-
-        for (r = inputStream.read(); r != '\n' && r != -1 ; r = inputStream.read()) {
-            baos.write(r);
-        }
-
-        if (r == -1 && baos.size() == 0) {
-            return "";
-        }
-
-        String lines = baos.toString("UTF-8");
-        return lines;
+        bufferedWriter.close();
     }
 }
